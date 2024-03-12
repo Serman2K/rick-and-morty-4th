5eef53ba8b4c2ebfb17bf75a7fd067b4f2da419e
Serman2K <57713379+Serman2K@users.noreply.github.com>
2024-03-12 20:00:46 +0100

    Create a static-view version of the site

--- src/Episodes/App.tsx
+++ src/Episodes/App.tsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Image from '../assets/image.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Header />
+      <p>Episodes of the <b>4th</b><br/> season of the series <br/><span className="cyan">Rick and Morty</span></p>
+      <img src={Image} className='image'/>
+      <Footer />
     </>
   )
 }
 
 export default App
+
+function Header() {
+  return (
+    <header>
+      LOREM IPSUM
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      LOREM IPSUM ©2021
+    </footer>
+  )
+}
