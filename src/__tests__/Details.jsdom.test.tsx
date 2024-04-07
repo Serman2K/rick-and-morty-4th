ce7ad90187f26a3e898fe72dbfe76a9fd60cb172
patryk.synowiecki <patryk.synowiecki@k2c.pl>
2024-04-08 01:37:22 +0200

    Fix undefined id in tests

--- src/__tests__/Details.jsdom.test.tsx
+++ src/__tests__/Details.jsdom.test.tsx
@@ -3,83 +3,93 @@ import { render, waitFor, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import { MockedProvider } from "@apollo/client/testing";
-import CharacterDetails, { DETAILS_QUERY } from "../CharacterDetails/CharacterDetails";
+import CharacterDetails, {
+  DETAILS_QUERY,
+} from "../CharacterDetails/CharacterDetails";
 
 jest.mock("react-router-dom", () => ({
-    ...jest.requireActual("react-router-dom"),
-    useNavigate: () => jest.fn(),
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+  useParams: () => ({
+    episodeId: "32",
+  }),
 }));
 
 describe("Details component", () => {
-    const mockDetails = {
-        request: {
-            query: DETAILS_QUERY,
+  const mockDetails = {
+    request: {
+      query: DETAILS_QUERY,
+      variables: {
+        episodeId: "32",
+      },
+    },
+    result: {
+      data: {
+        character: {
+          id: "1",
+          name: "Rick Sanchez",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          status: "Alive",
+          species: "Human",
+          type: "",
+          gender: "Male",
+          origin: {
+            name: "Earth (C-137)",
+          },
+          location: {
+            name: "Citadel of Ricks",
+          },
         },
-        result: {
-            data: {
-                character: {
-                    id: "1",
-                    name: "Rick Sanchez",
-                    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-                    status: "Alive",
-                    species: "Human",
-                    type: "",
-                    gender: "Male",
-                    origin: {
-                        name: "Earth (C-137)"
-                    },
-                    location: {
-                        name: "Citadel of Ricks"
-                    }
-                }
-            }
-        }
-    };
+      },
+    },
+  };
 
-    test("Render character details without errors", async () => {
-        render(
-            <MemoryRouter initialEntries={["/episode/32/character/1/details"]}>
-                <MockedProvider mocks={[mockDetails]} addTypename={false}>
-                        <CharacterDetails />
-                </MockedProvider>
-            </MemoryRouter>
-        );
+  test("Render character details without errors", async () => {
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={[mockDetails]} addTypename={false}>
+          <CharacterDetails />
+        </MockedProvider>
+      </MemoryRouter>
+    );
 
-        expect(await screen.findByText("Loading...")).toBeInTheDocument();
+    expect(await screen.findByText("Loading...")).toBeInTheDocument();
 
-        await waitFor(() => {
-            expect(screen.getByText("Rick Sanchez")).toBeInTheDocument();
-            expect(screen.getByText("Alive")).toBeInTheDocument();
-            expect(screen.getByText("Human")).toBeInTheDocument();
-            expect(screen.getByText("Male")).toBeInTheDocument();
-            expect(screen.getByText("Earth (C-137)")).toBeInTheDocument();
-            expect(screen.getByText("Citadel of Ricks")).toBeInTheDocument();
-        });
+    await waitFor(() => {
+      expect(screen.getByText("Rick Sanchez")).toBeInTheDocument();
+      expect(screen.getByText("Alive")).toBeInTheDocument();
+      expect(screen.getByText("Human")).toBeInTheDocument();
+      expect(screen.getByText("Male")).toBeInTheDocument();
+      expect(screen.getByText("Earth (C-137)")).toBeInTheDocument();
+      expect(screen.getByText("Citadel of Ricks")).toBeInTheDocument();
     });
+  });
 
-    test("Check back button", async () => {
-        const navigate = jest.fn();
-        jest.spyOn(require("react-router-dom"), "useNavigate").mockReturnValue(navigate);
+  test("Check back button", async () => {
+    const navigate = jest.fn();
+    jest
+      .spyOn(require("react-router-dom"), "useNavigate")
+      .mockReturnValue(navigate);
 
-        render(
-            <MemoryRouter initialEntries={["/episode/32/character/1/details"]}>
-                <MockedProvider mocks={[mockDetails]} addTypename={false}>
-                        <CharacterDetails />
-                </MockedProvider>
-            </MemoryRouter>
-        );
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={[mockDetails]} addTypename={false}>
+          <CharacterDetails />
+        </MockedProvider>
+      </MemoryRouter>
+    );
 
-        await waitFor(() => {
-            expect(screen.getByText("Characters")).toBeInTheDocument();
-        });
-        userEvent.click(screen.getByText("Characters"));
+    await waitFor(() => {
+      expect(screen.getByText("Characters")).toBeInTheDocument();
+    });
+    userEvent.click(screen.getByText("Characters"));
 
-        await waitFor(() => {
-            expect(navigate).toHaveBeenCalledWith("/episode/undefined/characters");
-        })
-    })
+    await waitFor(() => {
+      expect(navigate).toHaveBeenCalledWith("/episode/32/characters");
+    });
+  });
 
-    afterEach(() => {
-        jest.resetAllMocks();
-    })
-})
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+});
